17098a2d5197d78e6994061f17f594c11d9a3b09
Timo Paulssen <timonator@perpetuum-immobile.de>
2008-05-07 13:52:37 +0200

    added the "forgot cu?" checker... somewhat ugly, but seems to work in simple testcases!

--- liblojbansuggest.py
+++ liblojbansuggest.py
@@ -6,12 +6,19 @@ class lojbanNode:
     self.ltype = ltype
     self.children = []
 
+  def test(self, fun):
+    return fun(self) + sum(bla.test(fun) for bla in self.children)
+
   def __str__(self):
     return "%s=( %s )" % (self.ltype, " ".join(str(child) for child in self.children))
 
 class textNode:
   def __init__(self, text):
+    self.ltype = "text"
     self.text = text
+
+  def test(self, fun):
+    return fun(self)
 
   def __str__(self):
     return self.text
@@ -65,6 +72,30 @@ class parseTree:
       return ns
 
     self.rootNode = constructNode(pt)[0]
+
+    # make another, flat representation of the tree
+    self.nodes = {}
+    def sumDicts(d1, ds):
+      if isinstance(ds, dict):
+        ds = [ds]
+      if len(ds) == 1:
+        d2 = ds[0]
+        for k in d2.keys():
+          if k in d1:
+            d1[k].extend(d2[k])
+          else:
+            d1[k] = d2[k]
+        return d1
+      else:
+        return sumDicts(d1, sumDicts(ds[0], ds[1:]))
+
+    def collectNodes(node):
+      if isinstance(node, textNode):
+        return {"text": [node]}
+      else:
+        return sumDicts({node.ltype: [node]}, [collectNodes(n) for n in node.children])
+
+    self.nodes = collectNodes(self.rootNode)
 
   def __str__(self):
     return str(self.rootNode)
@@ -122,12 +153,19 @@ class lojbantext:
 
 class lojbansentence:
   def __init__(self, textdata):
+    self.td = textdata
+    # 1. run camxes to get a parse tree
     self.pt = call_camxes(textdata, ["-f"])[1].strip()
 
     self.pt = parseTree(self.pt)
 
-    # 1. run camxes to get a parse tree
+    self.sug = []
+
     # 2. run checkers...
+    for ch in ltcheckers:
+      self.sug.extend(ch(self.td, self.pt))
+
+    print "\n".join(str(a) for a in self.sug)
 
   def __str__(self):
     return "Lojbanic Sentence: %s" % self.pt
@@ -136,10 +174,55 @@ class unparsabletext:
   def __init__(self, textdata):
     self.td = textdata
 
+    self.sug = []
+
     # see what broke...
+    for ch in utcheckers:
+      self.sug.extend(ch(self.td, self.pt))
 
   def __str__(self):
     return "Unparsable Text: %s" % self.td
+
+###############################################################################
+# checkers look at the text or parse tree and check for known mistakes, making
+# suggestions if they are found.
+# they return a list of dicts with the keys "range", "mistake" and "suggestion"
+#
+
+ltcheckers = []
+utcheckers = []
+
+def ltcheck(fun):
+  ltcheckers.append(fun)
+  return fun
+
+def utcheck(fun):
+  utcheckers.append(fun)
+  return fun
+
+###############################################################################
+# checkers for lojban text
+#
+
+@ltcheck
+def forgotCuChecker(text, pt):
+  if "sentence" not in pt.nodes:
+    # there is no sentence node in the parse tree. Look for a selbri that has
+    # more than one brivla inside.
+    selbrinode = pt.nodes['selbri3'][0]
+    if selbrinode.test(lambda node: node.ltype == "BRIVLA") > 0:
+      return [{"range": [0, 10],
+               "mistake": "forgot cu",
+               "suggestion": "There is no bridi in this sentence, but there is a tanru. Did you maybe forget to use cu to seperate a sumti from the intended selbri?"}]
+  return []
+
+###############################################################################
+# checkers for unparsable text
+#
+
+#
+# end of checkers
+###############################################################################
 
 def analyze(text):
   pass
