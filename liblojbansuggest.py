3d3d77b44cfa53f07c98f7504ea88d813f748830
Timo Paulssen <timonator@perpetuum-immobile.de>
2008-05-07 03:18:49 +0200

    liblojbansuggest now parses the sentences into a python-object-tree and seperates parsable from unparsable text.

--- liblojbansuggest.py
+++ liblojbansuggest.py
@@ -1,53 +1,145 @@
 import subprocess
 import os, sys, signal
 
+class lojbanNode:
+  def __init__(self, ltype):
+    self.ltype = ltype
+    self.children = []
+
+  def __str__(self):
+    return "%s=( %s )" % (self.ltype, " ".join(str(child) for child in self.children))
+
+class textNode:
+  def __init__(self, text):
+    self.text = text
+
+  def __str__(self):
+    return self.text
+
+class parseTree:
+  def __init__(self, pt):
+
+    def constructNode(text):
+
+      def bracketRange(text):
+        bd = 0
+        for i in range(len(text)):
+          c= text[i]
+          if c == "(": bd += 1
+          if c == ")": 
+            bd -= 1
+            if bd == 0:
+              return i
+
+      text = text.strip()
+
+      ns = []
+
+      if "=(" in text:
+        while len(text) > 0 and "=(" in text:
+          # we got a lojbanNode!
+          n = lojbanNode(text[:text.find("=(")])
+
+          insideText = text[text.find("=(") + 1:]
+          insideText = insideText[:bracketRange(insideText) + 1]
+          insideText = insideText.strip()
+
+          if not insideText:
+            break
+
+          n.children = constructNode(insideText[1:-1])
+
+          remText = str(n)
+          while "  " in text:
+            text = text.replace("  ", " ")
+          text = text[:text.find(remText)] + text[text.find(remText) + len(remText):]
+
+          text = text.strip()
+
+          ns.append(n)
+          text = text.strip()
+      else:
+        # we got a textnode
+        ns.append(textNode(text))
+
+      return ns
+
+    self.rootNode = constructNode(pt)[0]
+
+  def __str__(self):
+    return str(self.rootNode)
+
 def find_camxes():
   return os.path.expanduser("~/lojban/lojban_peg_parser.jar")
 
-def call_camxes(text, arguments=[])
+def call_camxes(text, arguments=[]):
   camxesPath = find_camxes()
-  sp = subprocess.Popen(["java", "-jar", camxespath] + arguments,
+  sp = subprocess.Popen(["java", "-jar", camxesPath] + arguments,
                         stdin = subprocess.PIPE, stdout=subprocess.PIPE)
   sp.stdin.write(text)
   sp.stdin.write("\n")
   sp.stdin.close()
-  sp.stdout.readlines()
+  a = sp.stdout.readlines()
   sp.stdout.close()
-  sys.kill(sp.pid, signal.SIGTERM)
-
+  os.kill(sp.pid, signal.SIGTERM)
+  return a
 
 class lojbantext:
-  def __init__(self, textdata):
-    pass
+  def splittext(self):
+    # TODO: this must be easier
+    tds = self.td.split(" ")
+    def ion(ar, wo): # index or not
+      try:
+        return ar[1:].index(wo) + 1
+      except:
+        return len(ar)
+    sp = lambda ar: min(ion(ar, ".i"), ion(ar, "i"), ion(ar, "ni'o"))
+    sentences = []
+    while len(tds) > 0:
+      sentences.append(" ".join(tds[:sp(tds)]))
+      tds = tds[sp(tds):]
 
+    self.sent = sentences
+
+  def __init__(self, textdata):
+    # newlines are evil.
+    self.td = textdata.replace("\n", " ")
     # 1. break up the text at i and ni'o
+    self.splittext()
+
     # 2. seperate lu-quoted passages TODO: is this sensible?
+
+    self.items = []
+
     # 3. see what can be parsed (camxes -t)
-    #    
-    #    if there are unparsable sentences/parts:
-    #      1. TODO: figure out something sensible
-    #
-    #      n. remove the erroneous parts from the text
-    #      n+1. continue with the rest
-    #
-    #    if everything is parsable:
-    #      -> continue with 4.
-    #
-    # create a class lojbansentence for each sentence.
-    # create a class unparsabletext for each unparsable text.
+    for sent in self.sent:
+      if call_camxes(sent, ["-t"])[1].strip() == sent:
+        self.items.append(lojbansentence(sent))
+      else:
+        self.items.append(unparsabletext(sent))
+
+    print "\n".join([str(it) for it in self.items])
 
 class lojbansentence:
   def __init__(self, textdata):
-    pass
+    self.pt = call_camxes(textdata, ["-f"])[1].strip()
+
+    self.pt = parseTree(self.pt)
 
     # 1. run camxes to get a parse tree
     # 2. run checkers...
 
+  def __str__(self):
+    return "Lojbanic Sentence: %s" % self.pt
+
 class unparsabletext:
   def __init__(self, textdata):
-    pass
+    self.td = textdata
 
     # see what broke...
+
+  def __str__(self):
+    return "Unparsable Text: %s" % self.td
 
 def analyze(text):
   pass
