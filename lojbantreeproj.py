6acf83cd16103aec0afc09895cda7c22ce906ae4
Timo Paulssen <timonator@perpetuum-immobile.de>
2010-07-08 01:08:15 +0200

    made a mistake directly before releasing. bad idea.
    time to start doing unit tests.

--- lojbantreeproj.py
+++ lojbantreeproj.py
@@ -1,6 +1,6 @@
 from socket import *
 from select import *
-from tree import *
+from lojbansuggest.tree import *
 from sys import stderr
 from pprint import pprint
 from subprocess import Popen, PIPE
--- suggest.py
+++ suggest.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from lojbansuggest import lojbantext
+from lojbansuggest.suggest import lojbantext
 
 while True:
   lojbantext(raw_input("> "))
