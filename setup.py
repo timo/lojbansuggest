10dccae1bfa554f09eab0b2de7076adfb488ce64
Timo Paulssen <timonator@perpetuum-immobile.de>
2010-07-08 01:08:54 +0200

    bump version, change dependency url.

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup
 
 setup(
         name = "lojbansuggest",
-        version = "0.2",
+        version = "0.2.1",
         author = "Timo Paulssen",
         author_email = "timo+lojbansuggest@wakelift.de",
-        download_url = "http://wakelift.de/lojban/software/python/lojbansuggest-0.2.tar.gz",
+        download_url = "http://wakelift.de/lojban/software/python/lojbansuggest-0.2.1.tar.gz",
         license = "BSD",
         install_requires = ["lojbantools"],
         scripts = ["suggest.py",
                    "showlojbantree.sh",
                    "lojbantreeproj.py"],
         packages = ["lojbansuggest"],
-        dependency_links = ["http://wakelift.de/lojban/software/python/lojbantools-0.2.tar.gz"],
+        dependency_links = ["http://wakelift.de/lojban/software/"],
         
         classifiers = ["Development Status :: 3 - Alpha",
             "Environment :: Console",
